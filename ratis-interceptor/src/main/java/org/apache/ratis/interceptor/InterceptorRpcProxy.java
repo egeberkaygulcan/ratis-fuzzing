7d5d1706c139d99120f4dd311a344f2dbaa92020
egeberkaygulcan <egeberkaygulcan@gmail.com>
2023-12-06 04:43:04 +0100

    Update InterceptorRpcProxy.java

--- ratis-interceptor/src/main/java/org/apache/ratis/interceptor/InterceptorRpcProxy.java
+++ ratis-interceptor/src/main/java/org/apache/ratis/interceptor/InterceptorRpcProxy.java
@@ -1,5 +1,10 @@
 package org.apache.ratis.interceptor;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.squareup.okhttp.*;
+
 import org.apache.ratis.conf.RaftProperties;
 import org.apache.ratis.interceptor.comm.InterceptorMessage;
 import org.apache.ratis.protocol.RaftPeer;
@@ -7,6 +12,7 @@ import org.apache.ratis.util.PeerProxyMap;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 // The proxy initiates a communication to each peer and is used to communicate to that peer
 // We need the proxy to send messages that are not intercepted
@@ -32,6 +38,7 @@ public class InterceptorRpcProxy implements Closeable {
     }
 
     private final RaftPeer peer;
+    private OkHttpClient client = new OkHttpClient();
 
     public InterceptorRpcProxy(RaftPeer peer, RaftProperties raftProperties) {
         this.peer = peer;
@@ -42,7 +49,29 @@ public class InterceptorRpcProxy implements Closeable {
 
     public InterceptorMessage send(InterceptorMessage request) {
         // TODO:
-        //  [ ] need to figure out the right address to the peer and send a http request to that peer
+        //  [X] need to figure out the right address to the peer and send a http request to that peer
+        String address = this.peer.getAddress();
+        String json = request.toJsonString();
+
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        Request httpRequest = new Request.Builder()
+                .url(address)
+                .post(RequestBody.create(JSON, json))
+                .build();
+        Response response = null;
+        InterceptorMessage msg = null;
+        try {
+            response = client.newCall(httpRequest).execute();
+            if (response != null) {
+                msg = new InterceptorMessage.Builder().buildWithJsonString(response.body().string());
+            }
+        } catch (IOException e) {
+            // TODO: handle exception
+        }
+        if (msg != null) {
+            return msg;
+        }
+
         return null;
     }
 }
